fadbafa22e11e00c83755cd04cb41ce384fc7b68
Huỳnh Trần Hiếu Nhân <hieunhan3128@gmail.com>
2018-10-20 20:40:50 +0700

    commit

---
+++ main/__init__.py
@@ -0,0 +1,31 @@
+import urllib.request
+import codecs
+import urllib.parse
+from pip._vendor.appdirs import unicode
+from html.parser import HTMLParser
+from string import Template
+import re
+
+
+class CustomHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag=="a":
+            for attr in attrs:
+                if attr[0]=="href":
+                    f1.writelines("attribute:{} \n" .format(attr[1]))
+
+#     def handle_endtag(self, tag):
+#         f1.writelines("Encountered a end tag:%s \n" %(tag))
+
+#     def handle_data(self, data):
+#         f1.writelines("Encountered some data  :%s \n" %(data))
+data=[];
+url = 'https://tiki.vn/'
+parser= CustomHTMLParser();
+f1=codecs.open('./testfile.txt', 'w+', 'utf-8');
+f2=codecs.open('./orgfile.txt', 'w+', 'utf-8');
+
+with urllib.request.urlopen(url,None,2000) as response:
+   convertedHtml =response.read().decode('utf-8');
+   f2.write(convertedHtml)
+   parser.feed(convertedHtml);
