bac071a4759ce3857e0960ddbc57143980aadeb6
Huỳnh Trần Hiếu Nhân <hieunhan3128@gmail.com>
2018-10-20 21:27:15 +0700

    ko biet viet gi

--- main/__init__.py
+++ main/__init__.py
@@ -6,13 +6,20 @@ from html.parser import HTMLParser
 from string import Template
 import re
 
-
+class LinkFilter:
+    def __init__(self, links):
+        self.links=links
+    def Filter(self):
+        linksTuple = (self.links);
+        return linksTuple; 
+                
 class CustomHTMLParser(HTMLParser):
+    linkArray=[];
     def handle_starttag(self, tag, attrs):
         if tag=="a":
             for attr in attrs:
                 if attr[0]=="href":
-                    f1.writelines("attribute:{} \n" .format(attr[1]))
+                    self.linkArray.append(attr[1])
 
 #     def handle_endtag(self, tag):
 #         f1.writelines("Encountered a end tag:%s \n" %(tag))
@@ -24,8 +31,11 @@ url = 'https://tiki.vn/'
 parser= CustomHTMLParser();
 f1=codecs.open('./testfile.txt', 'w+', 'utf-8');
 f2=codecs.open('./orgfile.txt', 'w+', 'utf-8');
-
+f3 = codecs.open('./link.txt', 'w+', 'utf-8');
+                 
 with urllib.request.urlopen(url,None,2000) as response:
    convertedHtml =response.read().decode('utf-8');
    f2.write(convertedHtml)
    parser.feed(convertedHtml);
+   for link in parser.linkArray:
+        f3.writelines("{} \n".format(link) );
