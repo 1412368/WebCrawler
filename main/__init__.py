d6e630b72d17f27c555322e0aafa5f1a4041f87f
Huỳnh Trần Hiếu Nhân <hieunhan3128@gmail.com>
2018-10-20 23:01:11 +0700

    format url
    format url

--- main/__init__.py
+++ main/__init__.py
@@ -5,6 +5,7 @@ from pip._vendor.appdirs import unicode
 from html.parser import HTMLParser
 from string import Template
 import re
+from urllib.parse import urlparse
 
 class LinkFilter:
     def __init__(self, links):
@@ -20,6 +21,7 @@ class CustomHTMLParser(HTMLParser):
             for attr in attrs:
                 if attr[0]=="href":
                     self.linkArray.append(attr[1])
+                    
 
 #     def handle_endtag(self, tag):
 #         f1.writelines("Encountered a end tag:%s \n" %(tag))
@@ -27,15 +29,23 @@ class CustomHTMLParser(HTMLParser):
 #     def handle_data(self, data):
 #         f1.writelines("Encountered some data  :%s \n" %(data))
 data=[];
-url = 'https://tiki.vn/'
+url = 'https://www.lazada.vn/'
 parser= CustomHTMLParser();
 f1=codecs.open('./testfile.txt', 'w+', 'utf-8');
 f2=codecs.open('./orgfile.txt', 'w+', 'utf-8');
 f3 = codecs.open('./link.txt', 'w+', 'utf-8');
+f4 = codecs.open('./prefix.txt', 'w+', 'utf-8');
                  
-with urllib.request.urlopen(url,None,2000) as response:
+with urllib.request.urlopen(url,None,200) as response:
    convertedHtml =response.read().decode('utf-8');
    f2.write(convertedHtml)
    parser.feed(convertedHtml);
    for link in parser.linkArray:
-        f3.writelines("{} \n".format(link) );
+       url = urlparse(link);
+       f1.writelines("{} \n".format(url.geturl()));
+       f3.writelines("{} \n".format(url.netloc+url.path+url.params));
+
+f1.close();
+f2.close();
+f3.close();
+f4.close();
