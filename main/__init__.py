919d165e26a6085e4204270f678dbccc3e92fa72
Huỳnh Trần Hiếu Nhân <hieunhan3128@gmail.com>
2018-10-21 14:51:16 +0700

    partial finish with format link

--- main/__init__.py
+++ main/__init__.py
@@ -9,6 +9,8 @@ from urllib.parse import urlparse
 from multiprocessing import Queue
 from threading import Thread
 from _collections import deque
+from NormalizeUrl import NormalizeUrl
+from threading import Timer
 
 class LinkFilter:
     def __init__(self, links):
@@ -27,34 +29,37 @@ class CustomHTMLParser(HTMLParser):
                     
 
 data=[];
-url = 'https://www.lazada.vn/'
+url = 'https://lazada.vn'
 parser= CustomHTMLParser();
 f1=codecs.open('./testfile.txt', 'w+', 'utf-8');
 f2=codecs.open('./orgfile.txt', 'w+', 'utf-8');
 f3 = codecs.open('./link.txt', 'w+', 'utf-8');
 f4 = codecs.open('./queueURl.txt', 'w+', 'utf-8');
+f5 = codecs.open('./connectError.txt', 'w+', 'utf-8');
 
-def normalizeUrl(url):
-    regexCheckHead= re.compile("(www.)+");
-    if not(regexCheckHead.match(url)):
-        return "www."+url;
-    return url;
-
-def getLinkFromPage(url):        
-    with urllib.request.urlopen(url,None,200) as response:
-        convertedHtml =response.read().decode('utf-8');
-        f2.write(convertedHtml)
-        parser.feed(convertedHtml);
-        normalizedUrls = [];
-        for link in parser.linkArray:
-            url = urlparse(link);       
-            f1.writelines("{} \n".format(url.geturl()));
-            fullUrl= url.netloc+url.path+url.params;
-            fullUrl=normalizeUrl(fullUrl);
-            f3.writelines("{} \n".format(fullUrl));
-            normalizedUrls.append(fullUrl);
-        return normalizedUrls;
-
+def getLinkFromPage(url):
+    parsedUrl = urlparse(url);        
+    try: 
+        with urllib.request.urlopen(url,None,200) as response:
+            convertedHtml =response.read().decode('utf-8');
+            f2.write(convertedHtml)
+            parser.feed(convertedHtml);
+            normalizedUrls = [];
+            for link in parser.linkArray:
+                url = urlparse(link);       
+                f1.writelines("{} \n".format(url.geturl()));
+                fullUrl= url.netloc+url.path+url.params;
+                if (fullUrl !="")and (fullUrl !=None):
+                    normalizeUrl = NormalizeUrl(fullUrl,parsedUrl.netloc)
+                    fullUrl = normalizeUrl.convertUrl();
+                    f3.writelines("{} \n".format(fullUrl));
+                    normalizedUrls.append(fullUrl);
+            return normalizedUrls;
+    except :
+        print("ValueError");
+        print(url);
+        f5.writelines("{} \n".format(url));
+        return []
 def checkDuplicateUrl(connectQueue, url):
     for connectUrl in connectQueue:
         if url == connectUrl:
@@ -67,34 +72,19 @@ def addUrlToConnectQueue(connectQueue, urlArray):
             connectQueue.append(url);
 
 def controller(connectQueue):
-    url = connectQueue.popleft();
-    urlArray= getLinkFromPage(url);
-    addUrlToConnectQueue(connectQueue, urlArray);
-    
+#     while len(connectQueue)>0:
+    for i in range(0,10):
+        url = connectQueue.popleft();
+        urlArray= getLinkFromPage(url);
+        r = Timer(1.0, addUrlToConnectQueue(connectQueue, urlArray));
+        for url in connectQueue:
+            f4.writelines("{}\n".format(url))
+        f4.writelines("--------------------------------------------\n")
+        
 connectQueue = deque([url]);
 controller(connectQueue);
-
-for url in connectQueue:
-    f4.writelines("{}\n".format(url))
-
-# def worker():
-#     url = connectQueue.get();
-#     controller(connectQueue, url);
-#     connectQueue.task_done();
-#     
-# def createCrawler(crawlerNumber):
-#     for i in range(crawlerNumber):
-#         t = Thread(target=worker)
-#         t.daemon = True
-#         t.start()
-
-# connectQueue= Queue();
-# def init():        
-#     controller(connectQueue, url)
-#     connectQueue.join();
-# 
-# init();
 f1.close();
 f2.close();
 f3.close();
 f4.close();
+f5.close();
